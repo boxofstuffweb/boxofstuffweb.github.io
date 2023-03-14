9a42f6abd4e524c9004a9a884486c6a994c6357e
ChrisHammond <ChrisHammond@users.noreply.github.com>
2023-03-14 13:37:51 +0000

    deploy: b18205a3af7fdd9a82784941c6a3ceb919062e3e

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -1,67 +1,62 @@
 
 var documents = [{
     "id": 0,
-    "url": "/404.html",
+    "url": "https://www.boxofstuff.com/404.html",
     "title": "404",
     "body": "404 Page does not exist!Please use the search bar at the top or visit our homepage! "
     }, {
     "id": 1,
-    "url": "/about/",
-    "title": "About BoxOfStuff.com",
-    "body": "This domain name is for lease "
+    "url": "https://www.boxofstuff.com/about",
+    "title": "About boxofstuff.com",
+    "body": " boxofstuff. com is a sample blog template from Chris Hammond With the open source repository available on GitHub, you can easily fork the repository and create your own blog/website using GitHub Pages for hosting. Learn how to use boxofstuff "
     }, {
     "id": 2,
-    "url": "/about",
-    "title": "About BoxOfStuff.com",
-    "body": "What’s in the box? "
-    }, {
-    "id": 3,
-    "url": "/categories",
+    "url": "https://www.boxofstuff.com/categories",
     "title": "Categories",
     "body": ""
     }, {
-    "id": 4,
-    "url": "/",
+    "id": 3,
+    "url": "https://www.boxofstuff.com/",
     "title": "Home",
     "body": "      Featured:                                             All Stories:                                                             Leasing the Domain Name BoxOfStuff. com: A Smart Business Move              :       In the digital age, having a strong online presence is crucial for businesses. A domain name is the first step in establishing a professional and credible image for your brand. . . :                               03 Feb 2023                                                                                            Leasing a domain name              :       A domain name is a unique address that allows users to access a website on the internet. Leasing a domain name is a great option for businesses and individuals who. . . :                               25 Jan 2023                                                                                            BoxOfStuff. com domain name for sale / lease              :       This domain name is available to lease. Minimum 5 year term. Reach out for details. Do you have a box? Does it have stuff in it? Does it need a. . . :                               04 Jul 2021                                                                                            Loot boxes!              :       Loot Boxes:                               10 Feb 2021                                                                                            Welcome to BoxOfStuff. com!              :       Do you have a box? Does it have stuff in it? Does it need a domain name? You’ve come to the right place. But in order to unlock the power. . . :                               16 Nov 2020                                            "
     }, {
-    "id": 5,
-    "url": "/About",
+    "id": 4,
+    "url": "https://www.boxofstuff.com/About",
     "title": "",
     "body": ""
     }, {
-    "id": 6,
-    "url": "/redirects.json",
+    "id": 5,
+    "url": "https://www.boxofstuff.com/redirects.json",
     "title": "",
-    "body": "{“/About”:”/about”} "
+    "body": "{“/About”:”https://www. boxofstuff. com/about”} "
     }, {
-    "id": 7,
-    "url": "/robots.txt",
+    "id": 6,
+    "url": "https://www.boxofstuff.com/robots.txt",
     "title": "",
     "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
     }, {
-    "id": 8,
-    "url": "/lease-boxofstuff.com-a-smart-decision/",
+    "id": 7,
+    "url": "https://www.boxofstuff.com/lease-boxofstuff.com-a-smart-decision/",
     "title": "Leasing the Domain Name BoxOfStuff.com: A Smart Business Move",
     "body": "2023/02/03 - In the digital age, having a strong online presence is crucial for businesses. A domain name is the first step in establishing a professional and credible image for your brand on the internet. Choosing the right domain name can be a difficult and time-consuming task, but leasing a domain name such as BoxOfStuff. com can be a smart business decision. BoxOfStuff. com is a memorable, concise, and easy-to-spell domain name that accurately represents the content of your website. The name is highly descriptive and can be easily understood by a wide range of customers. It is perfect for an online retail store that sells a variety of products, as it conveys a sense of diversity and the idea that you have everything in one place. Leasing a domain name allows you to use it for a specified period of time without having to purchase it outright. This option is perfect for businesses that are just starting out and want to test the waters before making a long-term commitment. It also offers the advantage of not having to worry about renewing the domain name annually, as the leasing company will take care of that for you. Leasing BoxOfStuff. com can provide a significant boost to your online presence and help you reach a wider audience. The domain name is easy to remember, making it easier for customers to find your website when they search for products online. In addition, it is easy to type, making it easier for customers to navigate to your website. In conclusion, leasing the domain name BoxOfStuff. com is a smart business decision that can help you establish a strong online presence and reach a wider audience. The domain name is memorable, concise, and easy to spell, making it easy for customers to find and navigate to your website. With its descriptive and attractive name, BoxOfStuff. com is a great choice for any business looking to establish a professional online presence. "
     }, {
-    "id": 9,
-    "url": "/leasing-a-domain-name/",
+    "id": 8,
+    "url": "https://www.boxofstuff.com/leasing-a-domain-name/",
     "title": "Leasing a domain name",
     "body": "2023/01/25 - A domain name is a unique address that allows users to access a website on the internet. Leasing a domain name is a great option for businesses and individuals who want to use a specific domain name for a certain period of time without committing to a long-term purchase. When leasing a domain name, the lessor (the person who owns the domain) retains ownership of the domain, but grants the lessee (the person who is leasing the domain) the right to use the domain for a certain period of time. The lessee is responsible for paying a monthly or annual fee for the use of the domain. One suggested rate for leasing a domain name is $100 per month or $1000 per year. This rate can vary depending on the specific domain name and the length of the lease. For example, a popular or generic domain name may have a higher leasing rate than a lesser-known domain name. In terms of the lease agreement, it’s important to specify the length of the lease and the rights and responsibilities of both the lessor and lessee. The lessee should be granted the right to use the domain name for the specified period of time, and the lessor should retain the right to revoke the lease if the lessee violates any terms of the agreement. Additionally, it’s important to specify whether or not the lessee has the option to renew the lease after it expires. It’s also important to consider the transfer of ownership of the domain name as well. The lessee may not have the right to transfer ownership of the domain name to another party, and it’s important to specify this in the lease agreement. In conclusion, leasing a domain name can be a great option for businesses and individuals who want to use a specific domain name for a certain period of time without committing to a long-term purchase. A suggested rate for leasing a domain name is $100 per month or $1000 per year, but the rate can vary depending on the specific domain name and the length of the lease. It’s important to have a clear lease agreement that specifies the rights and responsibilities of both the lessor and lessee, as well as the length of the lease and the option to renew. "
     }, {
-    "id": 10,
-    "url": "/domain-for-sale/",
+    "id": 9,
+    "url": "https://www.boxofstuff.com/domain-for-sale/",
     "title": "BoxOfStuff.com domain name for sale / lease",
     "body": "2021/07/04 - This domain name is available to lease. Minimum 5 year term. Reach out for details. Do you have a box? Does it have stuff in it? Does it need a domain name? You’ve come to the right place. But in order to unlock the power of the box, you must make an offer that is reasonable. This domain is available for lease, inquire via twitter or email for details. Loot BoxesClothing BoxesFood BoxesFree Boxes"
     }, {
-    "id": 11,
-    "url": "/loot-boxes/",
+    "id": 10,
+    "url": "https://www.boxofstuff.com/loot-boxes/",
     "title": "Loot boxes!",
     "body": "2021/02/10 - Loot BoxesVideo games sell loot boxes. People really love them! (not really). The video game industry has turned video games into continuous money making machines by selling credits that can be used to buy loot boxes, and then ultimately turning them into lottery like devices to obtain addons, skins, weapons and such for additional game play options. Examples of Loot Boxes"
     }, {
-    "id": 12,
-    "url": "/welcome-to-boxofstuff/",
+    "id": 11,
+    "url": "https://www.boxofstuff.com/welcome-to-boxofstuff/",
     "title": "Welcome to BoxOfStuff.com!",
     "body": "2020/11/16 - Do you have a box? Does it have stuff in it? Does it need a domain name? You’ve come to the right place. But in order to unlock the power of the box, you must make an offer that is reasonable. This domain is available for lease, inquire below for details. Loot BoxesVideo games sell loot boxes. People really love them! (not really). The video game industry has turned video games into continuous money making machines by selling credits that can be used to buy loot boxes, and then ultimately turning them into lottery like devices to obtain addons, skins, weapons and such for additional game play options. Clothing BoxesNot sure what type of stitch you need to get your clothing fix? There are plenty of third parties now that will sell you services and clothing, so you never have to leave your house! Unless you want other people to see this clothing you buy. You can find stylists online that will send you items that you can try on, and only pay for the items that you choose to keep. Food BoxesNot sure what you want for dinner tonight? Not sure what ingredients you need to make your next meal? There are food boxes for that! Many of these boxes come with all the ingredients, and instructions for preparing a meal. "
     }];
